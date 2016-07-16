323d03d52a6636acc91392e678b5a8e92bba2dd0
sjackso3 <sheldonj22@gmail.com>
2016-07-16 06:59:56 -0700

    got rid of weight function. 4 funcs, 3 have same definition but are called during different conditions in stim()

--- main.c
+++ main.c
@@ -18,20 +18,17 @@ unsigned int** create_props(int neurs, int neur_props) {
 }
 void main() {
   int num_of_objs=3;
-  struct nobj_meta *nobj_props;
-
+  struct nobj_meta *nobj_props;  
+  /*   
+    NOBJ VARS
+  */
   //[object id][neur id][neur propert id]
   unsigned int ***nobjs;
   //[object id][neur id][to con neur id]
   unsigned int ***cons;
-  //[object id][neur id][variable] -- TBDOC / TBI
-  /* A variable length list that holds variables for the neurs that are affected
-     By neur behavior. To Be planned and coded */
-  unsigned int ***nvar;
-
   /* 
     conid is associated to sending neur
-    conid is sent to receiving neur along with sitm upon firing
+    conid is used by sending neur, sent to receiving neur along with sitm upon firing
     receiving neur using conid as an index to its weights array
     these are 'dynimcally' generated at load time and not specified in confi files
   */
@@ -39,6 +36,14 @@ void main() {
   unsigned int ***conids;
   //[object id][neur id][receiving weights]
   double ***weights;
+  //[object id][neur id][variable] -- TBDOC / TBI
+  /* A variable length list that holds variables for the neurs that are affected
+     By neur behavior. To Be planned and coded */
+  double ***nvar;
+  /*   
+    END NOBJ VARS
+  */
+
 
   nobj_props=malloc(num_of_objs * sizeof(struct nobj_meta));
   nobjs=malloc( num_of_objs *  sizeof(unsigned int**));
