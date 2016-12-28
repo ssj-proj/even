133ae4082d3fdb9c2b6103aafbefab727f2164b8
ssj <ssj@localhost.localdomain>
2016-12-28 12:23:42 -0500

    obj-util can fix header values for var files
      --TODO--
        *validate that num of neurs are consistent accross all obj files
        *better way to manipulate files and objects
        *random change obj (mutate)

--- obj-util.py
+++ obj-util.py
@@ -1,5 +1,4 @@
 import csv,sys,getopt
-#TODO - update file headers
 
 def change_all_rows(inputfile, replace_with,col,row_interest):
 	rowi=0
@@ -18,9 +17,43 @@ def change_all_rows(inputfile, replace_with,col,row_interest):
 
 	writer = csv.writer(open(inputfile, 'w'))
 	writer.writerows(lines)
+
+def fix_file_headers(inputfile):
+	content = []
+	num_headers=0
+	num_columns=0
+	num_rows=0
+	with open(inputfile,'r') as file:
+		for line in file:
+			content.append(line)
+			if ":" in line:
+				num_headers+=1
+				print line
+			if "," in line:
+				num_rows+=1
+				cols = line.count(",")+1
+				if(num_columns==0):
+					num_columns=cols
+				elif (num_columns!=0 and cols!=num_columns):
+					print("!!!!ERROR: inconsistent number of columns:"+inputfile)
+	print ("num of row: "+str(num_rows)+" num cols "+str(num_columns))
+	if ".var" in inputfile:
+		print "var file"
+		for i in xrange(0,num_headers+num_rows-1):
+			print "vp vs ["+line+"]"+ " -- "+str(i)
+			if "vp" in content[i]:
+				print "replacing vp"
+				content[i]="vp: "+str(num_columns)+"\n"
+			if "non" in content[i]:
+				content[i]="non: "+str(num_rows)+"\n"
+	with open(inputfile,'w') as file:
+		for line in content:
+			file.write(line)	
+			print line
+
 def append_column(inputfile, num, row_interest):
 	rowi=0
-	reader = csv.reader(open(inputfile )) # Here your csv file
+	reader = csv.reader(open(inputfile ))
 	lines = [l for l in reader]
 
 	for row in lines:
@@ -35,20 +68,21 @@ def append_column(inputfile, num, row_interest):
 
 	writer = csv.writer(open(inputfile, 'w'))
 	writer.writerows(lines)
-
+	fix_file_headers(inputfile)
+		
 def main(argv):
 	inputfile = ''
 	replace_with=""
 	col=1
 	row_interest=-1
 	rowi=0
-
+	validate=0
 	replace_col=0#set by R -> calls change_all_rows
 	append_col=0
 	help=0
 
 	try:
-		opts, args = getopt.getopt(argv,"f:r:c:aw:RA",[])
+		opts, args = getopt.getopt(argv,"f:r:c:aw:RAV",[])
 	except getopt.GetoptError:
 		print 'test.py -i <inputfile> -o <outputfile>'
 		sys.exit(2)
@@ -69,6 +103,8 @@ def main(argv):
 			append_col=1
 		elif opt == "-h":
 			help=1
+		elif opt == "-V":
+			validate=1
 	#print inputfile+" "+replace_with+" "+row_interest
 	if(help==1):
 		print "-f input file"
@@ -78,6 +114,7 @@ def main(argv):
 		print "-w replace with"
 		print "-R Replace column(s) - modified with either -r or -a"
 		print "-A append an column(S) to end of all rows"
+		print "-V validate header of file"
 		print "-h print help"
 		
 
@@ -86,6 +123,8 @@ def main(argv):
 		change_all_rows(inputfile,replace_with,col,row_interest)
 	if(append_col==1):
 		append_column(inputfile, 1,row_interest)
+	if(validate==1):
+		fix_file_headers(inputfile)
 	
 if __name__ == "__main__":
    main(sys.argv[1:])
