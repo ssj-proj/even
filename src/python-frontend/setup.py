a8d6558095094e828d46d62cf0591940ad3d0098
ssj <ssj@localhost.localdomain>
2017-02-04 12:21:09 -0500

    .

--- src/python-frontend/setup.py
+++ src/python-frontend/setup.py
@@ -1,6 +1,8 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+import os
+print(os.getcwd())
 
 setup(
 	cmdclass = {'build_ext': build_ext},
